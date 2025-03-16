68f22571fe6addf1537d856f33f1d8777bb942cf
HenryZumaeta <henryzumaeta@gmail.com>
2025-03-15 21:32:57 -0500

    Documentación de ValPBIAS

--- R/sesgo_robustez.R
+++ R/sesgo_robustez.R
@@ -116,3 +116,62 @@ ValMASE <- function(x, y) {
 
     return(list(mase = mase))
 }
+
+
+#' Calcula el Sesgo Porcentual (PBIAS)
+#'
+#' Esta función calcula el sesgo porcentual entre las predicciones y las observaciones.
+#' El PBIAS se expresa en porcentaje y mide la tendencia general del modelo a sobreestimar o subestimar
+#' los valores observados.
+#'
+#' @param x Data frame, matriz o vector numérico que contiene las predicciones.
+#' @param y Data frame, matriz o vector numérico que contiene las observaciones.
+#'
+#' @return Una lista con un elemento:
+#' \describe{
+#'   \item{pbias}{Matriz con el sesgo porcentual para cada combinación de modelo y observación, expresado en porcentaje.}
+#' }
+#'
+#' @details Para cada par de columnas correspondientes a un modelo, la función:
+#' \enumerate{
+#'   \item Convierte \code{x} e \code{y} a data frames.
+#'   \item Calcula la suma de las diferencias entre las predicciones y las observaciones.
+#'   \item Divide esta suma entre la suma de las observaciones y multiplica por 100 para obtener el valor en porcentaje.
+#' }
+#'
+#' @author
+#' Henry P. Zumaeta Lozano (\email{henry.zumaeta.l@uni.pe})\cr
+#' LinkedIn: \href{https://www.linkedin.com/in/henryzumaeta}{henryzumaeta}\cr
+#' WhatsApp: \href{https://wa.me/51963719768}{+51963719768}
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   # Ejemplo de uso:
+#'   pred <- data.frame(modelo1 = c(10, 12, 14), modelo2 = c(9, 11, 15))
+#'   obs <- data.frame(real1 = c(10, 11, 13), real2 = c(8, 10, 12))
+#'   resultado <- ValPBIAS(pred, obs)
+#'   print(resultado$pbias)
+#' }
+ValPBIAS <- function(x, y) {
+    x <- as.data.frame(x)
+    y <- as.data.frame(y)
+    numreal <- ncol(y)
+    numsim <- ncol(x)
+
+    pbias <- matrix(nrow = numreal, ncol = numsim,
+                    dimnames = list(paste("Observacion", 1:numreal),
+                                    paste("Modelo", 1:numsim)))
+
+    for (i in 1:numsim) {
+        for (j in 1:numreal) {
+            xx <- x[[i]]
+            yy <- y[[j]]
+            valid_indices <- which(!is.na(xx) & !is.na(yy))
+            pbias[j, i] <- 100 * (sum(xx[valid_indices] - yy[valid_indices]) / sum(yy[valid_indices]))
+        }
+    }
+
+    return(list(pbias = pbias))
+}
